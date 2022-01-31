26ebc0cacc3ce387ef1e201e6f1a8f20c529db17
Burgesskeith <keith.burgess@webwings.com.au>
2022-01-31 21:21:28 +1000

    Version 1 minor changes

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -10,8 +10,8 @@ function Banner() {
                     </div>
                     <div className="col-2">
                         <p>Exclusively Available on RedStore</p>
-                        <h1>Smart Band 4</h1>
-                        <small>The Mi Smart Band 4 fearures a 39.9%larger (than Mi Band 3) AMOLED color full-touch display
+                        <h1>Smart Band 5</h1>
+                        <small>The Mi Smart Band 5 fearures a 39.9%larger (than Mi Band 4) AMOLED color full-touch display
                             with adjustable brightness, so everything is clear as can be.<br /></small>
                         <a href="products.html" className="btn">Buy Now </a>
                     </div>
